5989dfbd05cf171b0db568d5b6dfed14e2a1846d
ZhanarTlepbayeva <tlepbaevazanar@gmail.com>
2023-01-28 16:40:55 +0600

    title metody kuryldy

--- demo.java
+++ demo.java
@@ -2,4 +2,7 @@ public class demo{
     public static void main(String[] args){
         System.out.printlb("Salem, Alem!");
     }
+    public static void main(String[] args){
+        System.out.printlb("Title");
+    }
 }
